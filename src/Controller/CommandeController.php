5ed0c3414fdbc762993f22cf836dc7740e716b21
julienM29 <miossec.julien@orange.fr>
2023-09-21 09:45:59 +0200

    mise en place de l'authentification , sans validation du compte par un email. Login - Logout et Registration

--- src/Controller/CommandeController.php
+++ src/Controller/CommandeController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Commande;
 use App\Form\CommandeFormType;
+use App\Repository\EtatRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,9 +12,11 @@ use Symfony\Component\Routing\Annotation\Route;
 class CommandeController extends AbstractController
 {
     #[Route('/commande', name: '_commande')]
-    public function index(): Response
+    public function index(EtatRepository $etatRepository): Response
     {
+        $etatCreer = $etatRepository->findOneBy(array('id' => 1));
         $commande = new Commande();
+        $commande->setEtat($etatCreer);
         $commandeForm = $this->createForm(CommandeFormType::class, $commande);
         return $this->render('commande/index.html.twig', compact('commandeForm'));
     }
