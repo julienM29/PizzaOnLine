14401639658ca690466abe609950151e8ab14f4c
julienM29 <miossec.julien@orange.fr>
2023-09-24 18:41:10 +0200

    début de css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    "./assets/**/*.js",
+    "./templates/**/*.html.twig",
+    "./node_modules/flowbite/**/*.js",
+  ],
   theme: {
     extend: {},
   },
   plugins: [
-      require('daisyui')
+      require('daisyui'),
+      require("flowbite"),
   ],
 }
--- webpack.config.js
+++ webpack.config.js
@@ -54,11 +54,11 @@ Encore
     })
 
     // enables Sass/SCSS support
-    //.enableSassLoader()
+    .enableSassLoader()
 
     // uncomment if you use TypeScript
     //.enableTypeScriptLoader()
-
+    .enablePostCssLoader()
     // uncomment if you use React
     //.enableReactPreset()
 
