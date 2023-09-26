60cdb18fe5cd1699af9b2925d2ef730453292638
julienM29 <miossec.julien@orange.fr>
2023-09-26 18:43:36 +0200

    Mise en place du leaflet routing machine, plus qu'à mettre en place la voiture et un set interval pour le faire bouger dessus et mettre en place une adresse de livraison quand un client passe une commande pour changer le marker d'arrivé.

--- assets/map.js
+++ assets/map.js
@@ -12,54 +12,47 @@ function affichageMapVille(){
         maxZoom: 19,
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright"></a>'
     }).addTo(map);
-    var greenIcon = L.icon({
-        iconUrl: '/images/pizzeria.png',
-        iconSize:     [50, 50], // size of the icon
-        iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
-        shadowAnchor: [4, 62],  // the same for the shadow
-        popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
-    });
-    L.marker([47.2264, -1.62076], {icon: greenIcon}).addTo(map);
     map.on('click', onMapClick);
-    affichagePointeurClient();
+    affichagePointeurs();
     affichageRoute();
+    cacherLesMarkerAuto();
 }
 
-function onMapClick(e) {
-    let str = "Lieu sélectionné"
-    let latitude = e.latlng.lat;
-    let longitude = e.latlng.lng;
-    let balise  = document.createElement("a");
-    balise.setAttribute("id", 'boutonCreaMap');
-    let createAText = document.createTextNode(str);
-    balise.appendChild(createAText);
-    popup
+function affichagePointeurs(){
+    var defaultIcon = L.icon({
+        iconUrl: './images/marker-icon.png',
+        iconSize: [25, 41],
+        iconAnchor: [12, 41],
+        popupAnchor: [0, -41]
+    });
 
-        .setLatLng(e.latlng)
-        .setContent(balise)
-        .openOn(map)
+// Configurez le marqueur par défaut de Leaflet
+    L.Marker.prototype.options.icon = defaultIcon;
 
-    let longitudeText = document.getElementById('lieu_longitude');
-    longitudeText.value = longitude;
-    let latitudeText =document.getElementById('lieu_latitude')
-    latitudeText.value = latitude;
-}
+// Créez vos marqueurs personnalisés
+    var pizzeriaIcon = L.icon({
+        iconUrl: '/images/pizzeria.png',
+        iconSize: [50, 50],
+        iconAnchor: [20, 40],
+        shadowAnchor: [4, 62],
+        popupAnchor: [-3, -76]
+    });
+    L.marker([47.2264, -1.62076], { icon: pizzeriaIcon }).addTo(map);
 
-function affichagePointeurClient(){
     var clientIcone = L.icon({
         iconUrl: '/images/client.png',
-        iconSize:     [50, 50],
-        iconAnchor:   [22, 94],
+        iconSize: [50, 50],
+        iconAnchor: [20, 40],
         shadowAnchor: [4, 62],
-        popupAnchor:  [-3, -76]
+        popupAnchor: [-3, -76]
     });
-    L.marker([47.22336, -1.62558], {icon: clientIcone}).addTo(map);
+    L.marker([47.22540, -1.63017], { icon: clientIcone }).addTo(map);
     var voitureIcone = L.icon({
         iconUrl: '/images/voiturePizza.png',
-        iconSize:     [50, 50],
-        iconAnchor:   [22, 94],
+        iconSize: [40, 40],
+        iconAnchor: [20, 40],
         shadowAnchor: [4, 62],
-        popupAnchor:  [-3, -76]
+        popupAnchor: [-3, -76]
     });
     L.marker([47.22537, -1.62304], {icon: voitureIcone}).addTo(map);
 }
@@ -85,4 +78,40 @@ function affichageRoute(){
     //     routeWhileDragging: false,
     //     show:false
     // }).addTo(map);
+}
+function onMapClick(e) {
+    let latitude = e.latlng.lat;
+    let longitude = e.latlng.lng;
+    let str = "latitude : " + latitude + "longitude : " + longitude ;
+
+    let balise  = document.createElement("a");
+    balise.setAttribute("id", 'boutonCreaMap');
+    let createAText = document.createTextNode(str);
+    balise.appendChild(createAText);
+    let popup = L.popup();
+
+    popup
+
+        .setLatLng(e.latlng)
+        .setContent(balise)
+        .openOn(map)
+
+    console.log('je suis dans le map on click');
+    let longitudeText = document.getElementById('longitude');
+    longitudeText.value = longitude;
+    let latitudeText =document.getElementById('latitude')
+    latitudeText.value = latitude;
+}
+function cacherLesMarkerAuto(){
+    var markersGroup = L.layerGroup();
+
+    var marker1 = L.marker([47.2264, -1.62076]);
+    var marker2 = L.marker([47.22540, -1.63017]);
+    markersGroup.addLayer(marker1);
+    markersGroup.addLayer(marker2);
+
+    markersGroup.eachLayer(function (marker) {
+        map.removeLayer(marker);
+    });
+
 }
